70f3076c59742de3f3ee2e3cc5d90ba9ececfb0e
Zachary Bouthillette <zachrbou@gmail.com>
2020-04-06 21:36:56 -0500

    added test cases

--- Quiz2.c
+++ Quiz2.c
@@ -23,3 +23,44 @@ int sumsTo(uint x[], uint n, uint k, uint v)
     int res2 = sumsTo(x+1, n-1, k, v);
     return res1 + res2 > 0;
 }
+
+int main()
+{
+    int one[7] = {0,1,2,3,4,5,6};
+    int two[3] = {4,8,1};
+    int three[15] = {4,15,65,34,7,2,74,2,6,8,2,45,724,12,4};
+    int four[0] = {};
+
+    if(sumsTo(one, 7, 3, 5))
+    {
+        printf("true\n");
+    }
+    else
+    {
+        printf("false\n");
+    }
+    if(sumsTo(two, 3, 3, 15))
+    {
+        printf("true\n");
+    }
+    else
+    {
+        printf("false\n");
+    }
+    if(sumsTo(three, 15, 2, 10))
+    {
+        printf("true\n");
+    }
+    else
+    {
+        printf("false\n");
+    }
+    if(sumsTo(four, 0, 3, 5))
+    {
+        printf("true\n");
+    }
+    else
+    {
+        printf("false\n");
+    }
+}
